b04dc1686fd3c1b1a5b821aaf34ef6da82ee7709
nguyenduyhung98 <nguyenduyhungdn161998@gmail.com>
2021-01-06 10:03:26 +0700

    add vetor icon

--- src/navigation/TabNavigatior.js
+++ src/navigation/TabNavigatior.js
@@ -5,14 +5,46 @@ import CategoryNavigatior from './CategoryNavigatior'
 import SettingNavigatior from './SettingNavigator'
 import ProfilesNavigatior from './ProfilesNavigatior'
 import  CardNavigatior  from './CardNavigatior'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigatior() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={CategoryNavigatior}/>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'information-circle-outline'
+                : 'information-circle-sharp';
+            } 
+            else if (route.name === 'CardOrder') 
+            {
+              iconName = focused
+                ? 'md-cart-outline'
+                : 'md-cart-sharp';
+            } 
+            else if (route.name === 'Profile') 
+            {
+              iconName = focused ? 'md-person-outline' : 'md-person-sharp';
+            } 
+            else if (route.name === 'Setting')
+            {
+              iconName = focused ? 'md-options-outline' : 'md-options-sharp';
+            }
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen name="Home" component={CategoryNavigatior} />
         <Tab.Screen name="CardOrder" component={CardNavigatior} />
         <Tab.Screen name="Profile" component={ProfilesNavigatior} />
         <Tab.Screen name="Setting" component={SettingNavigatior} />
